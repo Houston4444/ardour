b94f6464b3d4abee8b027eebdb0755b225a3d52a
Robin Gareus <robin@gareus.org>
2017-11-08 19:43:21 +0100

    Disk-Reader & Transport mess;  Work In Progress

--- libs/ardour/ardour/session.h
+++ libs/ardour/ardour/session.h
@@ -1447,7 +1447,6 @@ private:
 			PostTransportReverse|
 			PostTransportCurveRealloc|
 			PostTransportAudition|
-			PostTransportStop|
 			PostTransportClearSubstate);
 
 	gint _post_transport_work; /* accessed only atomic ops */
